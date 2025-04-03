87c4c2f6b6890373035e594406578d7521f34936
charmingruby <mrrubycanal3105@gmail.com>
2025-04-03 16:09:16 -0300

    feat: creates base actors

--- contracts/Consensus.sol
+++ contracts/Consensus.sol
@@ -5,6 +5,23 @@ pragma solidity ^0.8.28;
 // import "hardhat/console.sol";
 
 contract Consensus {
+    address private _manager;
+    mapping(uint8 => bool) private _groups;
+    mapping(address => uint8) private _groupLeaders;
+    mapping(address => bool) private _counselors;
+
+    constructor() {
+        _manager = msg.sender;
+
+        for (uint8 i = 1; i <= 10; i++) {
+            _groups[i] = true;
+        }
+    }
+
+    function getManager() public view returns (address) {
+        return _manager;
+    }
+
     function ping() public pure returns (string memory) {
         return "pong";
     }
