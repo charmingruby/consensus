6d00c867e5539903d7e2693bf40207b7e5d8d744
charmingruby <mrrubycanal3105@gmail.com>
2025-04-03 18:04:01 -0300

    feat: leaders management starter fns

--- contracts/Consensus.sol
+++ contracts/Consensus.sol
@@ -6,9 +6,9 @@ pragma solidity ^0.8.28;
 
 contract Consensus {
     address private _manager;
-    mapping(uint8 => bool) private _groups;
-    mapping(address => uint8) private _groupLeaders;
     mapping(address => bool) private _counselors;
+    mapping(address => uint8) private _leaders;
+    mapping(uint8 => bool) private _groups;
 
     constructor() {
         _manager = msg.sender;
@@ -18,11 +18,54 @@ contract Consensus {
         }
     }
 
-    function getManager() public view returns (address) {
-        return _manager;
+    function addLeader(
+        address _leader,
+        uint8 _groupId
+    ) external restrictedToCouncil {
+        require(groupExists(_groupId), "Group does not exists");
+
+        _leaders[_leader] = _groupId;
     }
 
-    function ping() public pure returns (string memory) {
-        return "pong";
+    function removeLeader(
+        address _leader,
+        uint8 _groupId
+    ) external restrictedToManager {
+        require(groupExists(_groupId), "Group does not exists");
+        require(isLeader(_leader), "Leader does not exists");
+
+        _leaders[_leader] = 0;
+    }
+
+    function groupExists(uint8 _groupId) public view returns (bool) {
+        return _groups[_groupId];
+    }
+
+    function isLeader(address _groupLeader) public view returns (bool) {
+        return _leaders[_groupLeader] > 0;
+    }
+
+    modifier restrictedToManager() {
+        require(
+            msg.sender == _manager,
+            "Only the manager can call this function"
+        );
+        _;
+    }
+
+    modifier restrictedToCouncil() {
+        require(
+            msg.sender == _manager || _counselors[msg.sender],
+            "Only the council members can call this function"
+        );
+        _;
+    }
+
+    modifier restrictedToGroupLeaders() {
+        require(
+            msg.sender == _manager || isLeader(msg.sender),
+            "Only the group leaders can call this function"
+        );
+        _;
     }
 }
--- test/Consensus.test.ts
+++ test/Consensus.test.ts
@@ -1,20 +1,107 @@
 import { loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
-import { expect } from "chai";
 import hre from "hardhat";
+import { expect } from "chai";
 
 describe("Consensus", () => {
   async function deployFixture() {
-    const [owner, otherAccount] = await hre.ethers.getSigners();
+    const [manager, groupMember] = await hre.ethers.getSigners();
 
     const Consensus = await hre.ethers.getContractFactory("Consensus");
-    const consensus = await Consensus.deploy();
+    const contract = await Consensus.deploy();
 
-    return { consensus, owner, otherAccount };
+    return { contract, manager, groupMember };
   }
 
-  it("should ping", async () => {
-    const { consensus, otherAccount, owner } = await loadFixture(deployFixture);
+  describe("Membership Management", () => {
+    describe("addLeader", () => {
+      it("should add a leader to a group", async () => {
+        const { contract, manager, groupMember } = await loadFixture(deployFixture);
 
-    expect(await consensus.ping()).to.equal("pong");
+        await contract.addLeader(groupMember.address, 1)
+
+        expect(await contract.isLeader(groupMember.address)).to.equal(true);
+      })
+
+      it("should be not able to add a leader to a group that does not exist", async () => {
+        const { contract, manager, groupMember } = await loadFixture(deployFixture);
+
+        await expect(contract.addLeader(groupMember.address, 11))
+          .to.be.revertedWith("Group does not exists");
+      })
+
+      it("should be not able to add a leader to a group if is not a council member", async () => {
+        const { contract, manager, groupMember } = await loadFixture(deployFixture);
+
+        const groupMemberContract = contract.connect(groupMember)
+
+        await expect(groupMemberContract.addLeader(groupMember.address, 1))
+          .to.be.revertedWith("Only the council members can call this function");
+      })
+    })
+
+    describe("removeLeader", () => {
+      it("should remove a leader from a group", async () => {
+        const { contract, manager, groupMember } = await loadFixture(deployFixture);
+
+        await contract.addLeader(groupMember.address, 1)
+
+        expect(await contract.isLeader(groupMember.address)).to.be.equal(true);
+
+        await contract.removeLeader(groupMember.address, 1)
+
+        expect(await contract.isLeader(groupMember.address)).to.be.equal(false);
+      })
+
+      it("should be not able to remove a leader from a group that does not exists", async () => {
+        const { contract, manager, groupMember } = await loadFixture(deployFixture);
+
+        await expect(contract.removeLeader(groupMember.address, 11))
+          .to.be.revertedWith("Group does not exists");
+      })
+
+      it("should be not able to remove a leader from a group if is not a manager", async () => {
+        const { contract, manager, groupMember } = await loadFixture(deployFixture);
+
+        await contract.addLeader(groupMember.address, 1)
+
+        expect(await contract.isLeader(groupMember.address)).to.be.equal(true);
+
+        const groupMemberContract = contract.connect(groupMember)
+
+        await expect(groupMemberContract.removeLeader(groupMember.address, 1))
+          .to.be.revertedWith("Only the manager can call this function");
+      })
+    })
+
+
+    describe("isLeader", () => {
+      it("should return true if the address is a leader", async () => {
+        const { contract, manager, groupMember } = await loadFixture(deployFixture);
+
+        await contract.addLeader(groupMember.address, 1)
+
+        expect(await contract.isLeader(groupMember.address)).to.equal(true);
+      })
+
+      it("should return false if the address is not a leader", async () => {
+        const { contract, manager, groupMember } = await loadFixture(deployFixture);
+
+        expect(await contract.isLeader(groupMember.address)).to.be.equal(false);
+      })
+    })
+
+    describe("groupExists", () => {
+      it("should return true if the group exists", async () => {
+        const { contract, manager, groupMember } = await loadFixture(deployFixture);
+
+        expect(await contract.groupExists(1)).to.equal(true);
+      })
+
+      it("should return false if the group does not exist", async () => {
+        const { contract, manager, groupMember } = await loadFixture(deployFixture);
+
+        expect(await contract.groupExists(11)).to.equal(false);
+      })
+    })
   })
 });
